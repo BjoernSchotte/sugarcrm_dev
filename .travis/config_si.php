21b1a4dca23ad73132f01fa3c0c2b67c3633c83b
Björn Schotte <bjoern.schotte@googlemail.com>
2012-09-15 18:40:27 +0200

    config_si.php for travis build

---
+++ .travis/config_si.php
@@ -0,0 +1,31 @@
+<?php
+# config_si for travis build
+#
+$sugar_config_si = array (
+    'setup_db_host_name' => 'localhost',
+    'setup_db_database_name' => 'sugarcrm',
+    'setup_db_drop_tables' => 0,
+    'setup_db_create_database' => 1,
+    'setup_db_pop_demo_data' => 'yes',
+    'setup_site_admin_password' => '${sugarpassword}',
+    'setup_db_create_sugarsales_user' => 1,
+    'setup_db_admin_user_name' => 'sugarcrm',
+    'setup_db_admin_password' => 'sugarcrm',
+    'setup_db_sugarsales_user' => 'root',
+    'setup_db_sugarsales_password' => 'root',
+    'setup_db_type' => 'mysql',
+    'setup_site_url' => 'http://localhost/',
+    # 'setup_system_name' => '${systemname}',
+    'default_currency_iso4217' => 'USD',
+    'default_currency_name' => 'US Dollars',
+    'default_currency_significant_digits' => '2',
+    'default_currency_symbol' => '$',
+    'default_date_format' => 'Y-m-d',
+    'default_time_format' => 'H:i',
+    'default_decimal_seperator' => '.',
+    'default_export_charset' => 'ISO-8859-1',
+    'default_language' => 'en_us',
+    'default_locale_name_format' => 's f l',
+    'default_number_grouping_seperator' => ',',
+    'export_delimiter' => ',',
+);
